19ff7b7d08c8b26ebf5e298dacbd5bf25d08eb51
6dotte <6dottechnologies@gmail.com>
2026-03-15 22:13:59 +0530

    Add WhiteNoiseMiddleware to MIDDLEWARE settings

--- mkbrothers/settings.py
+++ mkbrothers/settings.py
@@ -54,6 +54,7 @@ TAWKTO_IS_SECURE=True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
